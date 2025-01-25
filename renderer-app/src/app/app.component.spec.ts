ca7434e5f8782c10933911e3643725e2b053d1cf
Filipe Freire <filipe.freire32@gmail.com>
2025-01-25 21:44:30 +0000

    Change angular to jest tests

--- renderer-app/src/app/app.component.spec.ts
+++ renderer-app/src/app/app.component.spec.ts
@@ -2,10 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    });
   });
 
   it('should create the app', () => {
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'renderer-app in electron' title`, () => {
+  it('should have the title "renderer-app in electron"', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('renderer-app in electron');
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, renderer-app');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('renderer-app in electron');
   });
 });
